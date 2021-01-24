9f3a6f3d6177ff540ef126d3b42e836da56f6c39
SachinVarghese <smv@seldon.io>
2021-01-24 15:19:57 +0530

    Initial commit

---
+++ Code/Model Training/predict.py
@@ -0,0 +1,70 @@
+import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+import tensorflow.keras as keras
+
+# path to json file that stores MFCCs and genre labels for each processed segment
+DATA_PATH = "./dataset.json"
+
+
+def load_data(data_path):
+    """Loads training dataset from json file.
+
+        :param data_path (str): Path to json file containing data
+        :return X (ndarray): Inputs
+        :return y (ndarray): Targets
+    """
+
+    with open(data_path, "r") as fp:
+        data = json.load(fp)
+
+    # convert lists to numpy arrays
+    X = np.array(data["mfcc"])
+    y = np.array(data["labels"])
+
+    print("Data succesfully loaded!")
+    return X, y
+
+def predict(model, X, y):
+    """Predict a single sample using the trained model
+
+    :param model: Trained classifier
+    :param X: Input data
+    :param y (int): Target
+    """
+
+    # add a dimension to input data for sample - model.predict() expects a 4d array in this case
+    X = X[np.newaxis, ...]  # array shape (1, 130, 13, 1)
+    
+    # perform prediction
+    prediction = model.predict(X)
+    
+    # get index with max value
+    predicted_index = np.argmax(prediction, axis=1)
+
+    print("Target: {}, Predicted label: {}".format(y, predicted_index))
+
+
+if __name__ == "__main__":
+
+    # load data
+    X, y = load_data(DATA_PATH)
+
+    # create train/test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    model = keras.models.load_model('models/gunshot-detection/1/model.savedmodel')
+
+    # Check its architecture
+    model.summary()
+
+    # evaluate model on test set
+    test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
+    print('\nTest accuracy:', test_acc)
+
+    # pick a sample to predict from the test set
+    X_to_predict = X_test[3]
+    y_to_predict = y_test[3]
+    
+    # predict sample
+    predict(model, X_to_predict, y_to_predict)
---
+++ README.md
@@ -0,0 +1,2 @@
+
+# Project 15 - Elephant Listening Project
